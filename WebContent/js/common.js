5a5c984f92affe37a98470e878430ad5ef7294b0
yewon <ejsin28@gmail.com>
2020-12-22 11:09:59 +0900

    searchsection_js

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    //login + search div 일단 안보이게
     $("#login").hide();
     $("#search_div").hide();
 
@@ -7,12 +7,14 @@ $(document).ready(function() {
     $(".emphasized").hide();
     $(".backlayer").hide();
 
-/*
-        $("#popup_close").click(function() {
-            $(".emphasized").hide();
-            $(".backlayer").hide();
-        });
-*/
+    /*
+            $("#popup_close").click(function() {
+                $(".emphasized").hide();
+                $(".backlayer").hide();
+            });
+    */
+
+    // bean_button이 눌리면 bean_close로 src 바뀌면서 login section div 오픈
     $("#bean_button").click(function() {
         if ($("#bean_img").attr("src") == "img/bean.png" || $("#bean_img").attr("src") == "img/bean.PNG") {
             $("#bean_img").attr("src", "img/bean_close.PNG");
@@ -21,24 +23,37 @@ $(document).ready(function() {
         }
         $("#login").slideToggle(200);
 
-
-
     });
 
+    // search 버튼이 눌리면 search_close로 src 바뀌면서 search section div 오픈
     $("#search").click(function() {
-        console.log($("#search_img").attr("src")+"클릭 앞");
         if ($("#search_img").attr("src") == "img/search.PNG" || $("#search_img").attr("src") == "img/search.png") {
             $("#search_img").attr("src", "img/search_close.PNG");
         } else {
             $("#search_img").attr("src", "img/search.png");
         }
         $("#search_div").slideToggle(200);
-        console.log($("#search_img").attr("src")+"클릭 뒤");
     });
 
+    // search section div에서 form > submit 버튼 눌렸을 때 search_key 입력되었는지 확인
+    $("#search_form").submit(function(e) {
+        // 기본동작 수행 방식
+        e.preventDefault();
+
+        /** 검색어가 입력되었는지 검사 */
+        var target = $("#search_key");           // 대상요소를 가져온다.
+        console.log(target.val());
+        if (!target.val()) {                // 값이 없다면?
+            alert("검색어를 입력하세요.");    // 메시지 표시
+            target.focus();                 // 대상요소에게 포커스 강제 지정
+            return false;                   // 실패했음을 리턴
+        }
+        return true;                        // 성공했음을 리턴
+    });
+
+    // md's pick 슬라이드 쇼에서 이미지에 마우스 오버 시 quickview 클래스 보이게!
     $(".mdsimg").hover(
         function() {
-            console.log("hover 들어온다.");
             $(this).parent().parent().find(".quickview").toggleClass("hidden");
         }
     );
