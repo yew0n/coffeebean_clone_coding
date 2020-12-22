4302cfb5868d88a151062ca10a3573d8b2e3e0c8
yewon <ejsin28@gmail.com>
2020-12-22 13:24:11 +0900

    playBtn_css + popup_backLayer

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -3,16 +3,18 @@ $(document).ready(function() {
     $("#login").hide();
     $("#search_div").hide();
 
-    /* 팝업 일단 안뜨게 해놓음 > 나중에 이거 없애고 #popup_close.click 주석처리를 풀어야함 */
-    $(".emphasized").hide();
-    $(".backlayer").hide();
+    var height=$('#header').height()+$('#container').height()+$('#footer').height();
+    $(".backlayer").css('height',height+'px');
 
-    /*
-            $("#popup_close").click(function() {
-                $(".emphasized").hide();
-                $(".backlayer").hide();
-            });
-    */
+    /* 팝업 일단 안뜨게 해놓음 > 나중에 이거 없애고 #popup_close.click 주석처리를 풀어야함
+    $(".emphasized").hide();
+    $(".backlayer").hide();*/
+
+    $("#popup_close").click(function() {
+        $(".emphasized").hide();
+        $(".backlayer").hide();
+    });
+
 
     // bean_button이 눌리면 bean_close로 src 바뀌면서 login section div 오픈
     $("#bean_button").click(function() {
@@ -41,14 +43,14 @@ $(document).ready(function() {
         e.preventDefault();
 
         /** 검색어가 입력되었는지 검사 */
-        var target = $("#search_key");           // 대상요소를 가져온다.
+        var target = $("#search_key"); // 대상요소를 가져온다.
         console.log(target.val());
-        if (!target.val()) {                // 값이 없다면?
-            alert("검색어를 입력하세요.");    // 메시지 표시
-            target.focus();                 // 대상요소에게 포커스 강제 지정
-            return false;                   // 실패했음을 리턴
+        if (!target.val()) { // 값이 없다면?
+            alert("검색어를 입력하세요."); // 메시지 표시
+            target.focus(); // 대상요소에게 포커스 강제 지정
+            return false; // 실패했음을 리턴
         }
-        return true;                        // 성공했음을 리턴
+        return true; // 성공했음을 리턴
     });
 
     // md's pick 슬라이드 쇼에서 이미지에 마우스 오버 시 quickview 클래스 보이게!
